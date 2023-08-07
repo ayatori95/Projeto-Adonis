0357771ae3bfe011b52697cbf5e1714a950b7c6d
Ayatori <lorenacristinaac@gmail.com>
2023-08-07 17:41:25 -0300

    Introdução a Models e Migrations. Foi instalada a extensão lucid para a criação de modelos

--- test.ts
+++ test.ts
@@ -18,10 +18,24 @@ import 'reflect-metadata'
 import sourceMapSupport from 'source-map-support'
 import { Ignitor } from '@adonisjs/core/build/standalone'
 import { configure, processCliArgs, run, RunnerHooksHandler } from '@japa/runner'
+import execa from 'execa'
 
 sourceMapSupport.install({ handleUncaughtExceptions: false })
 
 const kernel = new Ignitor(__dirname).kernel('test')
+
+async function runMigrations() {
+  await execa.node('ace', ['migration:run'], {
+    stdio: 'inherit',
+  })
+}
+
+async function rollbackMigrations() {
+  await execa.node('ace', ['migration:rollback'], {
+    stdio: 'inherit',
+  })
+}
+
 
 kernel
   .boot()
@@ -37,6 +51,8 @@ kernel
         importer: (filePath) => import(filePath),
         setup: app.concat(runnerHooks.setup),
         teardown: runnerHooks.teardown,
+        before: runMigrations,
+        after: rollbackMigrations,
       },
       cwd: kernel.application.appRoot,
     })
