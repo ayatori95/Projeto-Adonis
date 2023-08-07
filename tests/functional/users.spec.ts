208f96fe835be76fe3318c7dcf15b82380094e29
Ayatori <lorenacristinaac@gmail.com>
2023-08-07 17:14:45 -0300

    Testetando falha de componente user

--- tests/functional/users.spec.ts
+++ tests/functional/users.spec.ts
@@ -6,14 +6,15 @@ const BASE_URL = `http://${process.env.HOST}:${process.env.PORT}`;
 
 test.group('Users', () => {
   test('it shoub be able to create a new user', async () => {
-    const user = {
+    const userPayload = {
       name: 'John Doe',
       email: 'test@test.com',
       password: 'test' }
-    const {body} = await supertest(BASE_URL).post('/users').send(user).expect(201)
+    const {body} = await supertest(BASE_URL).post('/users').send(userPayload).expect(201)
     chai.assert.exists(body.user, 'User undefined')
     chai.assert.exists(body.user.id,'Token undefined')
-    chai.assert.exists(body.user.email,'Email undefined')
-
+    chai.assert.equal(body.user.email, userPayload.email)
+    chai.assert.equal(body.user.name, userPayload.name)
+    chai.assert.equal(body.user.password,userPayload.password)
   })
 })
